e032af992a7f010506dd5cd806c74fb879dd64ef
Caio Marquezini <105600461+caion90@users.noreply.github.com>
2026-07-03 20:10:11 -0300

    Add files via upload
    created with visual code and platform.io

---
+++ src/main.cpp
@@ -0,0 +1,228 @@
+#include <ESP8266WiFi.h>
+#include <WiFiServer.h>
+#include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <painlessMesh.h>
+
+#ifndef STASSID
+#define STASSID "Internet 2.4G"
+#define STAPSK "internet"
+#endif
+
+WiFiClient espClient;
+WiFiServer wifiServer(2222);
+PubSubClient client(espClient);
+#define MSG_BUFFER_SIZE (50)
+unsigned long lastMsg = 0;
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+const char *ssid = STASSID;
+const char *password = STAPSK;
+const char *host = "OTA-LEDS";
+const char *mqtt_server = "debian.local";
+
+int led_pin = 13;
+#define N_DIMMERS 3
+int dimmer_pin[] = {14, 5, 15};
+
+int countTokens(const char *input, char delimiter)
+{
+  if (input == nullptr)
+  {
+    return 0;
+  }
+
+  int count = 0;
+  bool inToken = false;
+
+  for (int i = 0; input[i] != '\0'; ++i)
+  {
+    if (input[i] == delimiter)
+    {
+      if (inToken)
+      {
+        count++;
+        inToken = false;
+      }
+    }
+    else
+    {
+      inToken = true;
+    }
+  }
+
+  return inToken ? count + 1 : count;
+}
+
+int tokenize(char *input, char delimiter, char *tokens[], int maxTokens)
+{
+  if (input == nullptr || tokens == nullptr || maxTokens <= 0)
+  {
+    return 0;
+  }
+
+  int tokenCount = 0;
+  char *start = input;
+
+  for (char *p = input; *p != '\0'; ++p)
+  {
+    if (*p == delimiter)
+    {
+      *p = '\0';
+      if (start != p)
+      {
+        if (tokenCount < maxTokens)
+        {
+          tokens[tokenCount++] = start;
+        }
+        else
+        {
+          break;
+        }
+      }
+      start = p + 1;
+    }
+  }
+
+  if (start != input && *start != '\0')
+  {
+    if (tokenCount < maxTokens)
+    {
+      tokens[tokenCount++] = start;
+    }
+  }
+
+  return tokenCount;
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  if ((char)payload[0] == '1')
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, HIGH); // Turn the LED off by making the voltage HIGH
+  }
+}
+
+void log(char *logmsg)
+{
+  Serial.println(logmsg);
+}
+
+void reconnect()
+{
+  while (!client.connected())
+  {
+    log("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str()))
+    {
+      client.publish("outTopic", "hello world");
+      client.subscribe("inTopic");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(led_pin, OUTPUT);
+  digitalWrite(led_pin, LOW);
+
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    WiFi.begin(ssid, password);
+  }
+
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  analogWriteRange(1000);
+  analogWrite(LED_BUILTIN, 990);
+
+  for (int i = 0; i < N_DIMMERS; i++)
+  {
+    pinMode(dimmer_pin[i], OUTPUT);
+    analogWrite(dimmer_pin[i], 50);
+  }
+
+  ArduinoOTA.setHostname(host);
+  ArduinoOTA.onStart([]()
+                     {
+    for (int i = 0; i < N_DIMMERS; i++)
+    {
+      analogWrite(dimmer_pin[i], 0);
+    }
+    analogWrite(LED_BUILTIN, 0); });
+
+  ArduinoOTA.onEnd([]()
+                   {
+    for (int i = 0; i < 30; i++)
+    {
+      analogWrite(LED_BUILTIN, (i * 100) % 1001);
+      delay(50);
+    } });
+
+  ArduinoOTA.onError([](ota_error_t error)
+                     {
+    (void)error;
+    ESP.restart(); });
+
+  ArduinoOTA.begin();
+  wifiServer.begin();
+  client.setCallback(callback);
+}
+
+int Update_Loop_Count = 0;
+
+void loop()
+{
+  WiFiClient server = wifiServer.available();
+  if (server)
+  {
+    while (server.connected())
+    {
+      while (server.available() > 0)
+      {
+        char received = server.read();
+        if (countTokens(&received, ',') > 0)
+        {
+          switch (received)
+          {
+          case '0':
+            digitalWrite(LED_BUILTIN, LOW);
+            log("led off");
+            break;
+          case '1':
+            digitalWrite(LED_BUILTIN, HIGH);
+            log("led on");
+            break;
+          default:
+            log("Unknown command received");
+            break;
+          };
+          delay(10);
+        }
+        server.stop();
+      }
+      ArduinoOTA.handle();
+    };
+  }
+};
